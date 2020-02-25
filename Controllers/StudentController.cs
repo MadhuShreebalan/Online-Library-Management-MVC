db945db08bfd6d280768ce9fb78881352e3e50e7
madhushreen2020 <madhushreen2020@srishakthi.ac.in>
2020-02-25 15:18:29 +0530

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using LibraryManagement.Repository;
+using LibraryManagement.Entity;
+
+namespace LibraryManagement.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+        StudentRepository studentRepository;
+        public StudentController()
+        {
+            studentRepository = new StudentRepository();
+        }
+        // GET: Index
+        public ActionResult Index()
+        {
+            IEnumerable<Student> students = studentRepository.GetAllStudents();
+            return View(students);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Student students)
+        {
+            studentRepository.AddStudent(students);
+            TempData["Message"] = "Employee Added Successfully!";
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(double id)
+        {
+            studentRepository.DeleteStudent(id);
+            TempData["Message"] = "Employee Deleted Successfully!";
+            return RedirectToAction("Index");
+        }
+        public ActionResult Edit(double id)
+        {
+            Student students = studentRepository.GetStudent(id);
+            return View(students);
+        }
+        [HttpPost]
+        public ActionResult Update(Student students)
+        {
+            studentRepository.UpdateStudent(students);
+            TempData["Message"] = "Employee Details Updated Successfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
