117c0b61e109fd1c43d85271fabe28fd4a00ff42
madhushreen2020 <madhushreen2020@srishakthi.ac.in>
2020-03-04 09:05:07 +0530

    code-first approach in Entity Framework

--- Models/LibraryManagementContext.cs
+++ Models/LibraryManagementContext.cs
@@ -3,7 +3,7 @@ using System.Data.Entity;
 namespace LibraryManagement.Models
 {
     public class LibraryManagementContext : DbContext
-    {
+    { 
         public LibraryManagementContext() : base("name=LibraryManagementConnection") { }
         public DbSet<CategoryViewModel> Category { get; set; }
 
